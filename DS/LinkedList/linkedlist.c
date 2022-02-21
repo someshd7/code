bf98288b792dbb040f971cfa91629851c9255cc0
somesh deshmukh <33504689+someshd7@users.noreply.github.com>
2022-02-21 22:32:09 +0530

    Added insert at begining and insert at end functions.

--- DS/LinkedList/linkedlist.c
+++ DS/LinkedList/linkedlist.c
@@ -8,6 +8,71 @@ typedef struct{
     struct node *link
 }node;
 
+//Function to insert a node at the begin of the linked list.
+struct Node *insertAtBegining(struct Node *head, int x) {
+
+    /* insert at begining
+    create a new node
+    new_node->data = x;
+    new_node->link = NULL
+    Check if the head is NULL
+    if true, head is the new_node
+    if false, new_node->next = head and head = new_node;
+    */
+    struct Node *new_node = (struct Node*)malloc(sizeof(struct Node));
+    
+    new_node->data = x;
+    new_node->next = NULL;
+    if (head == NULL)
+    {
+        head = new_node;
+    }
+    else
+    {
+        new_node->next = head;
+        head           = new_node;
+    }
+    return head;
+}
+
+//Function to insert a node at the end of the linked list.
+struct Node *insertAtEnd(struct Node *head, int x)  {
+
+    /* Insert at end
+       Two nodes are required. One to attach at the end and one to 
+       traverse the linked list.
+       setup the new node with data and next to NULL.
+       Check if head is NULL
+       if true, only one node in the LL. return new node.
+       if not, temp node = head
+       traverse the temp node till temp->next is not NULL
+       Now you are at last node, temp->next = new_node
+
+       return head.
+    */
+    struct Node *new_node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *t = (struct Node*)malloc(sizeof(struct Node));
+    
+    new_node->data = x;
+    new_node->next = NULL;
+    
+    if (head == NULL)
+    {
+        return new_node;
+    }
+    
+    else
+    {
+        t = head;
+	    while(t->next != NULL)
+	    {
+	        t = t->next;
+	    }
+	    t->next = new_node;
+    }
+	return head;
+}
+
 void traverse (struct node* start)
 {
     /* Traverse the linked list and print the data from each node 
